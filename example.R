01b12ffce0b45a50be154485d2a9b32d8e9f8e45
Matt Pike <matt.pike@levellingup.gov.uk>
2023-07-27 12:09:35 +0100

    Prepared example csv and R script

--- example.R
+++ example.R
@@ -1,4 +1,13 @@
-# Example script
-var <- "Hello world!"
-print(var)
-print(typeof(var))
+# Example analysis script
+
+file_path <- "D:/Users/matt.pike/OneDrive - MHCLG/Desktop/Source Code/git-example-repo/git-teach-in/example_data.csv"
+df <- readr::read_csv(file_path)
+
+male_count <- count(df$GENDER == "M")
+female_count <- count(df$GENDER == "F")
+unidentified_count <- count(df$GENDER == "U")
+
+# Corrected version of the formatted string
+formatted_string <- paste("In this dataset there are", male_count, "males and",
+                          female_count, "females.", unidentified_count, "people are unidentified.")
+print(formatted_string)
