eff44765ace4ab43c973cdcf2896c8f15a6bccd5
Matt Pike <matt.pike@levellingup.gov.uk>
2023-07-27 12:19:50 +0100

    Corrected file path and comments

--- example.R
+++ example.R
@@ -1,13 +1,15 @@
-# Example analysis script
+### Example analysis script ###
 
-file_path <- "D:/Users/matt.pike/OneDrive - MHCLG/Desktop/Source Code/git-example-repo/git-teach-in/example_data.csv"
+# Read local csv containing a dummy dataset
+file_path <- "example_data.csv"
 df <- readr::read_csv(file_path)
 
+# Count the genders from the dataframe
 male_count <- count(df$GENDER == "M")
 female_count <- count(df$GENDER == "F")
 unidentified_count <- count(df$GENDER == "U")
 
-# Corrected version of the formatted string
+# Summarise the gender count in a formatted string
 formatted_string <- paste("In this dataset there are", male_count, "males and",
                           female_count, "females.", unidentified_count, "people are unidentified.")
 print(formatted_string)
