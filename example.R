43c6082466cc5e68d1c973d8cc64c046a201296a
Matt Pike <matt.pike@levellingup.gov.uk>
2023-07-31 10:23:57 +0100

    test gitignore and linting

--- example.R
+++ example.R
@@ -1,5 +1,10 @@
 ### Example analysis script ###
 
+
+library(dplyr)
+library(readr)
+
+
 # Read local csv containing a dummy dataset
 file_path <- "example_data.csv"
 df <- readr::read_csv(file_path)
@@ -10,6 +15,14 @@ female_count <- sum(df$GENDER == "F")
 unidentified_count <- sum(df$GENDER == "U")
 
 # Summarise the gender count in a formatted string
-formatted_string <- paste("In this dataset there are", male_count, "males and",
-                          female_count, "females.", unidentified_count, "people are unidentified.")
+formatted_string <-
+  paste(
+    "In this dataset there are",
+    male_count,
+    "males and",
+    female_count,
+    "females.",
+    unidentified_count,
+    "people are unidentified."
+  )
 print(formatted_string)
